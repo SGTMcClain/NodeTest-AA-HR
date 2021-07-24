46240edbcf75fae0992d5d2731b73fa05174aa04
Nathan McClain <n8mcclain@gmail.com>
2021-07-24 09:46:07 -0400

    I'm confused by the test error...
    it looks like the .then isn't working with a
    ".then isn't a function error" but I am not sure that I can fix that

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const writeJSON = () => {
+var fs = require('fs');
+const dataStore = require('./data-store');
 
+
+const writeJSON = (path, jsonfile) => {
+    const json = JSON.stringify(jsonfile)
+    console.log(json)
+    return path
 };
 
-const createDirectoryIfNotExists = () => {
 
-};
+const createDirectoryIfNotExists = (path) => {
+
+    if(!fs.existsSync(path)){
+        fs.mkdirSync(path)
+        console.log('created folder')
+    }
+    console.log('failed to create folder')
+    return path
+}
+
 
 
 module.exports = {
+    
     // Do not modify the names of the exports
     writeJSON: writeJSON,
     createDirectoryIfNotExists: createDirectoryIfNotExists
